5930de7f63f6b3847723d0da8650e9dfbcbaa85e
emitsianis <ev.mitsianis@gmail.com>
2018-09-09 21:38:27 +0300

    added mongoose models

---
+++ models/AqChars.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const aqcharSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  points: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  visits: {
+    type: Array,
+    required: true,
+    default: [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false
+    ]
+  }
+});
+
+module.exports = AqChars = mongoose.model("aqchars", aqcharSchema);
